1b0fa6778e9d444959ea3658fa78e3e138fcf63f
kanayade <yamichan_30@yahoo.ne.jp>
2026-04-14 02:15:07 +0900

    first commit

---
+++ src/database/migrations/2026_04_13_234937_create_attendance_corrections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceCorrectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_corrections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->datetime('corrected_start_time')->nullable();
+            $table->datetime('corrected_end_time')->nullable();
+            $table->string('status');
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_corrections');
+    }
+}
